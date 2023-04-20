1af34a7bd66c1afa1143b2cfdad4ffbe4349c5d2
marzzy16 <114106672+marzzy16@users.noreply.github.com>
2023-04-20 14:45:24 -0500

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Properties_Sawyer_Moira
+{
+    class Car
+    {
+
+        private string model;
+
+        public string Model { get { return model; } set { model = value; } }
+
+        public string Make { get; set; }
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Properties_Sawyer_Moira
+ 
